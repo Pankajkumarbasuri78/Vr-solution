ff0d9587871a651b6ad1c905b4193e2eb49a91be
Pankaj Basuri <pbasuri10078@gmail.com>
2023-03-21 21:16:01 +0530

    changes

--- src/components/Aboutus/Aboutus.jsx
+++ src/components/Aboutus/Aboutus.jsx
@@ -26,6 +26,7 @@ const TopMenu = styled.div(props => ({
   flexDirection:'row',
   justifyContent:'center',
   alignItems:'center',
+  gap:'35px',
   marginTop:'7.6rem',//122px
   marginBottom:'10rem'//218px
 }))
@@ -60,6 +61,7 @@ const BottomMenu =styled.div(props => ({
   flexDirection:'row',
   justifyContent:'center',
   alignItems:'center',
+  gap:'50px',
   marginBottom:'7.6rem',//122px
 }))
 const LeftContent = styled.div(props => ({
@@ -70,6 +72,93 @@ const RightImage = styled.div(props => ({
 }))
 const Image2 = styled.div(props => ({
   
+}))
+
+const Features = styled.div(props => ({
+  display:'flex',
+  flexDirection:'column',
+  flexBasis:'80%',
+
+}))
+const Row1 = styled.div(props => ({
+  display:'flex',
+  flexDirection:'row',
+  marginTop:'89px',
+  justifyContent:'space-between',
+  alignItems:'center',
+
+}))
+
+const Content1 = styled.div(props => ({
+  display: 'flex',
+  flexDirection: 'row',
+  gap: '10px',
+  paddingBottom: '1rem',
+  borderBottom: '1px solid #ccc',
+  position: 'relative',
+  //width: '225px',
+  width:'61%',
+
+  '&::before': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: '1px',
+    height: '100%',
+    backgroundColor: '#ccc',
+    transform: 'translateX(-50px)',
+  }
+}));
+const Content2 = styled.div(props => ({
+  display:'flex',
+  flexDirection:'row',
+  gap:'10px',
+  paddingTop:'1rem',
+  position: 'relative',
+
+  '&::before': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: '1px',
+    height: '100%',
+    backgroundColor: '#ccc',
+    transform: 'translateX(60px)',
+  }
+
+}))
+const Content3 = styled.div(props => ({
+  display:'flex',
+  flexDirection:'row',
+  gap:'10px',
+  paddingBottom:'1rem',
+  borderBottom:'1px solid #ccc',
+
+}))
+const Content4 = styled.div(props => ({
+  display:'flex',
+  flexDirection:'row',
+  gap:'8px',
+  paddingTop:'1rem',
+
+}))
+const Title = styled.div(props => ({
+  fontFamily:'Roboto',
+  fontSize:'1rem',
+  fontWeight:500,
+  lineHeight:'124%',
+  letterSpacing:'0.05rem',
+  color:'#01147A',
+  textTransform:'capitalize',
+
+}))
+const Row2 = styled.div(props => ({
+  display:'flex',
+  flexDirection:'row',
+  justifyContent:'space-between',
+  alignItems:'center',
 }))
 
 
@@ -102,9 +191,34 @@ export const Aboutus = (props) => {
             <Desc style={{ fontWeight: 400 }}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa pellentesque erat lectus cursus quisque faucibus vulputate. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa pellentesque erat lectus cursus quisque faucibus vulputate.
             </Desc>
-            <Image1>
+
+            {/* <Image1>
               <img src='/image/who.png' alt='img' />
-            </Image1>
+            </Image1> */}
+
+            <Features>
+              <Row1>
+                <Content1>
+                  <img src='/image/qualityJob.png' alt='icon' style={{height:'1.37rem',width:'1.37rem'}}/>
+                  <Title>Quality Job</Title>
+                </Content1>
+                <Content3>
+                  <img src='/image/qualityJob.png' alt='icon' style={{height:'1.37rem',width:'1.37rem'}}/>
+                  <Title>Top Companies</Title>
+                </Content3>
+              </Row1>
+
+              <Row2>
+                <Content2>
+                  <img src='/image/qualityJob.png' alt='icon' style={{height:'1.37rem',width:'1.37rem'}}/>
+                  <Title>No Charges</Title>
+                </Content2>
+                <Content4>
+                  <img src='/image/qualityJob.png' alt='icon' style={{height:'1.37rem',width:'1.37rem'}}/>
+                  <Title>International Job</Title>
+                </Content4>
+              </Row2>
+            </Features>
           </LeftContent>
 
           <RightImage>
