c7905c2a970516e7c68485c471319ba1c6c24ae5
Pankaj Basuri <pbasuri10078@gmail.com>
2023-03-22 22:39:29 +0530

    IndustryPage done

--- src/components/Pages/IndustryPage.jsx
+++ src/components/Pages/IndustryPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Heading } from '../common/Heading/Heading'
 import { Experties } from '../Experties/Experties'
 import { Footer } from '../Footer/Footer'
-import { Hero } from '../Hero/Hero'
+import { HeroIndustry } from '../HeroIndustry/HeroIndustry'
+//import { Hero } from '../Hero/Hero'
 
 export const IndustryPage = () => {
   return (
     <>
-        <Hero image='/image/industryPic.png'/>
+        <HeroIndustry image='/image/industryPic.png'/>
+        {/* <Hero image='/image/industryPic.png'/> */}
         <Heading name='OUR EXPERTIES'/>
         <Experties />
         <Footer />
